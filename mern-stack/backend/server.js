a0ea55148b638ca5e109bba5c55a6565a58cdf59
devilzzz-lab <srisuji0814@gmail.com>
2026-03-28 12:33:21 +0530

    Testing 5

--- mern-stack/backend/server.js
+++ mern-stack/backend/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+// ✅ Allow frontend origin
+app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
